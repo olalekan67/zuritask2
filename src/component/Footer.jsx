6a82b61eddf58e094a1033fa6bc60e3956c22602
bunyamin <bunyaminusalam@gmail.com>
2022-10-31 04:22:32 +0100

    i edited the Links dection and Footer section

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { FaGithub, FaSlack } from 'react-icons/fa'
+import i4gLogo from './assets/I4G.png'
+import zuriLogo from './assets/Vector.png'
 
 function Footer() {
   return (
-    <div className='d-flex flex-row justify-content-center gap-3 gap-md-4 mt-5'>
-        <a href="http://github.com/olalekan67" className='text-dark fs-3' target="_blank" rel="noreferrer"><FaGithub /></a>
-        <FaSlack className='text-dark fs-3 mt-2'/>
+    <div className='container mt-5'>
+        <hr />
+        <div className='d-flex flex-column flex-md-row justify-content-between'>
+            <img src={zuriLogo} alt="" className='img-fluid h-25 mb-2 mt-1 col-4 col-md-2' />
+            <p className='fs-6 text-secondary'>HNG Intership 9 Frontend Task</p>
+            <img src={i4gLogo} alt="" className='img-fluid h-25  col-4 col-md-2'/>
+        </div>
     </div>
+    
   )
 }
 
--- src/component/Links.jsx
+++ src/component/Links.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
+import { FaGithub, FaSlack } from 'react-icons/fa'
 
 function Links() {
   return (
-    <div>
-        <a href="https://twitter.com/olalekan103" target='_blank' rel='noreferrer'>
-            <button className='btn w-100' id='twitter'>Twitter Link</button>
-        </a>
-        <a href="https://training.zuri.team/"  target='_blank' rel='noreferrer'> 
-            <button className="btn w-100" id='btn_zuri'>Zuri Team</button>
-        </a>
-        <a href="http://books.zuri.team"  target='_blank' rel='noreferrer'>
-             <button className="btn w-100" id='books'>Zuri Books</button>
-        </a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=<Bunyamin Abdulsalam>"  target='_blank' rel='noreferrer'>
-             <button className="btn w-100" id='book_python'>Python Books</button>
-        </a>
-        <a href="https://background.zuri.team"  target='_blank' rel='noreferrer'>
-             <button className="btn w-100" id='pitch'>Background Check for Coders</button>
-        </a>
-        <a href="https://books.zuri.team/design-rules"  target='_blank' rel='noreferrer'>
-             <button className="btn w-100" id='book_design'>Design Books</button>
-        </a>
-    </div>
+     <section>
+          <div>
+               <a href="https://twitter.com/olalekan103" target='_blank' rel='noreferrer'>
+                    <button className='btn w-100' id='twitter'>Twitter Link</button>
+               </a>
+               <a href="https://training.zuri.team/"  target='_blank' rel='noreferrer'> 
+                    <button className="btn w-100" id='btn_zuri'>Zuri Team</button>
+               </a>
+               <a href="http://books.zuri.team"  target='_blank' rel='noreferrer'>
+                    <button className="btn w-100" id='books'>Zuri Books</button>
+               </a>
+               <a href="https://books.zuri.team/python-for-beginners?ref_id=<Bunyamin Abdulsalam>"  target='_blank' rel='noreferrer'>
+                    <button className="btn w-100" id='book_python'>Python Books</button>
+               </a>
+               <a href="https://background.zuri.team"  target='_blank' rel='noreferrer'>
+                    <button className="btn w-100" id='pitch'>Background Check for Coders</button>
+               </a>
+               <a href="https://books.zuri.team/design-rules"  target='_blank' rel='noreferrer'>
+                    <button className="btn w-100" id='book_design'>Design Books</button>
+               </a>
+     </div>
+          <div className='d-flex flex-row justify-content-center gap-3 gap-md-4 mt-2'>
+               <a href="http://github.com/olalekan67" className='text-dark fs-3' target="_blank" rel="noreferrer"><FaGithub /></a>
+               <FaSlack className='text-dark fs-3 mt-2'/>
+     </div>
+     </section>
+    
   )
 }
 
