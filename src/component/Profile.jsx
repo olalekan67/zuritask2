e1b53e559fe4c1779cfa474af0384bdd8c3ee2f8
olalekan67 <76757243+olalekan67@users.noreply.github.com>
2022-11-01 14:40:10 +0100

    Update Profile.jsx

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import profileImg from './assets/profile-img.jpg'
+import { FaShare } from 'react-icons/fa'
 import { TbDots } from 'react-icons/tb'
 
 function Profile({name}) {
@@ -11,7 +12,8 @@ function Profile({name}) {
         <h5 id='twitter' className='d-none'>olalekan103</h5>
         <h5 className="d-none" id='slack'>{name}</h5>
         <div className='position-top ms-auto'>
-          <button className='btn btn-outline-secondary col-1 profile-btn'><TbDots /></button>
+          <button className='btn btn-outline-secondary col-1 profile-btn d-md-none'><TbDots /></button>
+          <button className='btn btn-outline-secondary col-1 profile-btn d-none d-md-block'><FaShare /></button>
         </div>
     </div>
   )
